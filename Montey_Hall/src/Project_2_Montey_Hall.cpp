525fdd42e3a1b5a81c37dd30bd9bad09e2eaa1a1
Rehan Ashraf <rashraf.cheema@gmail.com>
2018-06-17 23:04:04 -0700

    Finished MonteyHall
    Signed-off-by: Rehan Ashraf <rashraf.cheema@gmail.com>

--- Montey_Hall/src/Project_2_Montey_Hall.cpp
+++ Montey_Hall/src/Project_2_Montey_Hall.cpp
@@ -21,31 +21,85 @@ typedef int Door;
 
 const int SIMULATION_COUNT = 100;
 
-void simulate(int, int, int );
+void simulate(int, int &, int &);
 
 Door hideCar();
 
-Door openDoor();
+Door openDoor(Door firstchoice, Door hiddencar);
 
 Door makeFirstChoice();
 
-Door makeSecondChoice( );
+Door makeSecondChoice(Door firstchoice, Door openeddoor );
 
 int main()
 {
     int win1 = 0, win2 = 0;
+    srand((unsigned)time(0));
 
     // Run the simulations.
     for (int i = 1; i <= SIMULATION_COUNT; i++)
-    	{
-    	simulate(i, win1, win2);
-    	}
-
+    {
+           	simulate(i, win1, win2);
+        cout << i << endl;
+    }
+    cout << "Win one is " << win1 << endl << "Win two is " << win2 << endl;
 
 }
 
-
-void simulate(int i, int win1, int win2 )
+Door hideCar()
 {
-	return;
+    return rand() % 3 + 1 ;
+}
+
+Door makeFirstChoice()
+{
+    return rand() % 3 + 1;
+}
+
+Door makeSecondChoice(Door firstchoice, Door openeddoor )
+{
+    Door thirddoor = 6 - firstchoice - openeddoor;
+//    int newchoice = rand() % 2;
+//    if (newchoice)
+//        return firstchoice;
+//    else
+//        return thirddoor;
+    return thirddoor;
+}
+
+Door openDoor(Door firstchoice, Door hiddencar)
+{
+
+    if( firstchoice != hiddencar)
+        return 6 -firstchoice - hiddencar;
+    else
+    {
+        Door DoortoOpen = rand() % 2 + 1;
+        int holdingarray[2] = {0} ;
+        for(int i = 0, j = 0; j < 2 ; i++)
+        {
+            if ( i != firstchoice )
+            {
+                holdingarray[j] = i;
+                j++;
+            }
+
+        }
+        return holdingarray[DoortoOpen];
+    }
+
+}
+
+void simulate(int i, int &win1, int &win2 )
+{
+    Door firstChoice, winningDoor, secondChoice, openedDoor;
+    winningDoor = hideCar();
+    firstChoice = makeFirstChoice();
+    openedDoor = openDoor(firstChoice,winningDoor);
+    secondChoice = makeSecondChoice(firstChoice, openedDoor);
+    if (winningDoor == firstChoice)
+        win1++;
+    else if (winningDoor == secondChoice)
+        win2++;
+    return;
 }
